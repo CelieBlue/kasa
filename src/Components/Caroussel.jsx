7074f1c92cae3b9c73a28c3d53f24d1bcbdc10db
Celine Clovel <c.clovel@gmail.com>
2023-01-23 15:08:01 -0400

    Ajout d'un useState et des constante currImg et SetCurrimg pour afficher l'image courage, et gérer la préc. et la suiv. - Ajout de la fonction onClick pour gérer la longueur du tableau et empêcher d'aller au-delà de la première image et la dernière image

--- src/Components/Caroussel.jsx
+++ src/Components/Caroussel.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
+import React, {useState}  from 'react'
 import "../Styles/Caroussel.css"
+import arrow_left from "../assets/arrow_left.png"
+import arrow_right from "../assets/arrow_right.png"
 
 export default function Caroussel ({images}) {
 
-    // const [currImg, setCurrImg] = useState(0);
+    const [currImg, setCurrImg] = useState(0);
 
     return (
-        
-        <div className="caroussel">
-            <div className="carousselInner">
-            { images.map((image, index) => {
-                return (
-                    <img  key={index} src={image} alt="caroussel_logement"/>
-                )
-            })}
-            </div>
+        <div className="carousel">
+            <div className="carousselInner"
+             style={{ backgroundImage: `url(${images[currImg]})`}}>
             
+                <div className="left" onClick={() => {
+                currImg > 0 && setCurrImg(currImg - 1);
+                }}>
+                    <img src={arrow_left} alt="Suivante"/>
+                </div>
 
-                        
-            {/* style={{ backgroundImage: `url(${images[currImg].img})`}}> */}
-
-
-            {/* </div> */}
+                <div className="right" onClick={() => {
+                    currImg < images.length -1 && setCurrImg(currImg + 1);
+                }}>
+                    <img src={arrow_right} alt="Précédente"/>
+                </div>
+            </div>
         </div>
+
     )
 }
